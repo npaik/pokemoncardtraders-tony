f26ed3c035dd6117cfa24e62b81e928c6dde2391
Tony Paik <nakhyunpaik@gmail.com>
2023-12-19 16:08:41 -0800

    Refactor card flipping functionality

--- pokemoncardtraders/src/App.jsx
+++ pokemoncardtraders/src/App.jsx
@@ -32,7 +32,10 @@ function App() {
   const handleCardBackClick = () => {
     console.log("Backside of card clicked");
     const shuffled = cards.sort(() => 0.5 - Math.random());
-    const selected = shuffled.slice(0, 5);
+    const selected = shuffled.slice(0, 5).map((card) => ({
+      ...card,
+      flipped: false, // Set the flipped state to false initially
+    }));
     setSelectedCards(selected);
 
     const cardIds = selected.map((card) => card.id);
@@ -58,6 +61,14 @@ function App() {
       .catch((error) => console.error("Error updating user cards:", error));
   };
 
+  const handleCardFlip = (index) => {
+    setSelectedCards((currentSelected) =>
+      currentSelected.map((card, idx) =>
+        idx === index && !card.flipped ? { ...card, flipped: true } : card
+      )
+    );
+  };
+
   return (
     <>
       <h1>Pokemon Cards</h1>
@@ -69,13 +80,30 @@ function App() {
         />
       </div>
       <div className="selected-cards-container">
-        {selectedCards.map((card) => (
-          <div key={card.id} className="selected-card">
-            <h3>{card.cardname}</h3>
-            <img src={card.cardurl} alt={`Pokemon card ${card.cardname}`} />
+        {selectedCards.map((card, index) => (
+          <div
+            key={`${card.id}-${index}`}
+            className="selected-card"
+            onClick={() => handleCardFlip(index)}
+          >
+            <div className={`card-inner ${card.flipped ? "flipped" : ""}`}>
+              <div className="card-front">
+                <img src={card.cardurl} alt={`Pokemon card ${card.cardname}`} />
+                <h3>{card.cardname}</h3>
+              </div>
+              <div className="card-back">
+                <img
+                  src="/pokemoncardback.jpg"
+                  alt="Backside of Pokemon card"
+                />
+              </div>
+            </div>
           </div>
         ))}
       </div>
+
+      {/* have this section in a separate page
+
       <h2>My Selected Cards</h2>
       <div className="user-selected-cards-container">
         {userSelectedCards.map((card, index) => (
@@ -84,7 +112,9 @@ function App() {
             <img src={card.cardurl} alt={`Pokemon card ${card.cardname}`} />
           </div>
         ))}
-      </div>
+      </div> 
+      
+      */}
     </>
   );
 }
